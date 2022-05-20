b3255e66e47458943e0783337f4ad390a1f17266
fullfox <fullfox@medusa.fr>
2022-05-20 14:24:35 +0200

    filename max display length

--- log.php
+++ log.php
@@ -7,7 +7,7 @@ $bdd = json_decode($log, true);
 foreach ($bdd as $randomname => $elem) {
   if($elem['link_private'] == 'false'){
     echo '<span class="item">' . $elem['ip'] . " uploaded <a href=\"download.php?d=" . $randomname . "\">"
-    . $elem['filename'] . "</a> (" . formatSizeUnits($elem['size']) . ") on "
+    . "<span class='filename'>" . $elem['filename'] . "</span></a> (" . formatSizeUnits($elem['size']) . ") on "
     . $elem['date'] . "</span>\n\n";
   }
 }
