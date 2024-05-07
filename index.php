cbbfa612b62e0bfad118d4cdc69bec2e6bfa9464
MDCDEV <me@mdcdev.me>
2024-05-07 00:11:10 -0500

    feat: Updated index.php

--- index.php
+++ index.php
@@ -1,26 +1,32 @@
 <?php include 'vars.php'; ?>
 <!DOCTYPE html>
-<html lang="fr" dir="ltr">
+<html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>filedropper.eu</title>
+    <title><?=$name ?></title>
+	<meta name="description" content="Save your large files and share them around the world.">
     <link rel="icon" type="image/png" href="favicon.png" />
-    <link rel="stylesheet" href="styles.css" media="screen and (min-device-width: 800px)">
-    <link rel="stylesheet" href="styles.css" media="screen and (max-device-width: 800px)">
+    <link rel="stylesheet" href="styles.css">
   </head>
   <body>
-    <h1>>filedropper.eu<span class="blink">_</span></h1>
+    <h1>><?=$name ?><span class="blink">_</span></h1>
     <?php if($info != ''){ ?><h2 class="info"><?=$info ?></h2><?php } ?>
 
-    <input type="hidden" name="MAX_FILE_SIZE" value="5000000000" />
-    <label class="checkbox">🕵 Keep my IP private <input type="checkbox" name="ip_private"/></label>
+    <input type="hidden" name="MAX_FILE_SIZE" value="<?=$MAX_FILE_SIZE ?>" />
     <label class="checkbox">🔐 Keep my link private <input type="checkbox" name="link_private"></label>
+    <label class="time">🕒 Link expiration <select name="time">
+      <option value="0">Never</option>
+      <option value="1">1 day</option>
+      <option value="7" selected>1 week</option>
+      <option value="30">1 month</option>
+      <option value="365">1 year</option>
+    </select></label>
 
     <input name="file" type="file" id="file-input"/>
     <label for="file-input" id="file-label" draggable="true">
     <div id="upload-box">
       <span id="dynamic-status">Click or drag a file here</span>
-      <span id="static-status"></span>
+      <span id="static-status">Max file size: <?=$MAX_FILE_SIZE/1024/1024 ?>MB</span>
     </div>
     </label>
     <br>
@@ -30,7 +36,6 @@
         <div class="th">filename</div>
         <div class="th">size</div>
         <div class="th">date</div>
-        <div class="th">IP</div>
       </a>
       <div id="file-list">
       </div>
@@ -38,6 +43,13 @@
 
     <br><br>
     <a href="https://github.com/fullfox/filedropper"><p><img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" width="28px" class="logo">fullfox</p></a>
+	  
+	<div class="footer-links">
+      <a href="https://files.mdcdev.me/support/kb/faq.php?id=3">Terms of Service</a> - 
+      <a href="https://files.mdcdev.me/support/kb/faq.php?id=2">Privacy Policy</a> - 
+      <a href="https://files.mdcdev.me/support/kb/faq.php?id=1">DMCA Policy</a> - 
+      <a href="https://files.mdcdev.me/support/index.php">Support</a>
+    </div>
 
     <script src="index.js" charset="utf-8"></script>
   </body>
