d99f596e728bb54776d986c5a628f9c43dec235a
MDCDEV <me@mdcdev.me>
2024-05-07 00:08:25 -0500

    feat: Dont need second check anymore

--- check.php
+++ check.php
@@ -4,8 +4,5 @@ include 'vars.php';
 echo "Is local directory writable for the webserver ? ";
 if(is_writable('.')) {echo 'yes';} else {echo 'no';}
 
-echo "<br> Is logfile writable for the webserver ? ";
-if(is_writable($logfile)) {echo 'yes';} else {echo 'no';}
-
-echo "<br>Make sure both are writable. If not, edit permission.";
+echo "<br>Make sure are writable. If not, edit permission.";
 ?>
