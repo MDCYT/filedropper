8ad176d8f129ae95e6a6bf3d66038a4644cf9341
MDCDEV <me@mdcdev.me>
2024-05-07 00:08:43 -0500

    feat: Delete only delete old and unnused files

--- delete.php
+++ delete.php
@@ -1,14 +1,49 @@
 <?php
 include 'vars.php';
 
-if(hash('sha256', $_GET["p"] . 'filedropper.eu') == $password){
-  $files = glob($tmp . '*');
-  foreach($files as $file){
-    if(is_file($file)) {
+// Check the files in the database
+$sql = "SELECT * FROM files";
+
+$result = $conn->query($sql);
+
+$files = glob($tmp . '*');
+
+$deleted = array();
+
+// If the file dont exist in the database, delete it, if the expiration_date is less than the actual date and is not null, delete it
+if ($result->num_rows > 0) {
+  while ($row = $result->fetch_assoc()) {
+    $filename = $row['filename'];
+    $randomname = $row['uuid'];
+    $filepath = $tmp . $randomname;
+    $expiration_date = $row['expiration_date'];
+
+    if ($expiration_date != null && strtotime($expiration_date) < time()) {
+      if (file_exists($filepath)) {
+        array_push($deleted, $filename." expired");
+        unlink($filepath);
+      }
+      $sql = "DELETE FROM files WHERE uuid = '$randomname'";
+      $conn->query($sql);
+    }
+
+    if (!file_exists($filepath)) {
+      $sql = "DELETE FROM files WHERE uuid = '$randomname'";
+      $conn->query($sql);
+    }
+  }
+
+  foreach ($files as $file) {
+    $filename = basename($file);
+    $sql = "SELECT * FROM files WHERE uuid = '$filename'";
+    $result = $conn->query($sql);
+    if ($result->num_rows == 0) {
+      array_push($deleted, $filename." not in database");
       unlink($file);
     }
   }
-  file_put_contents($logfile, "");
 }
+
+echo json_encode($deleted);
+
 die();
-?>
